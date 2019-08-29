ee53dd6d3aee125bf6a61f010b0f3ff40c2978c9
Frands Andersen <frands1995@gmail.com>
2019-08-29 13:55:37 +0200

    Accumulator added

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,29 +8,35 @@ namespace Calculator
 {
     public class CalculatorClass
     {
+        public double Accumulator { get; private set; }
+
         public CalculatorClass()
         {
         }
 
         public double Add(double a, double b)
         {
-            return a + b;
+            Accumulator =  a + b;
+            return Accumulator;
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            Accumulator = a - b;
+            return Accumulator;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            Accumulator = a * b;
+            return Accumulator;
         }
 
 
         public double Power(double x, double exp)
         {
-            return Math.Pow(x, exp);
+            Accumulator = Math.Pow(x, exp);
+            return Accumulator;
         }
 
         static void Main(string[] args)
